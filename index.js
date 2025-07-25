6913befe3b0b172d6a4b24991f8250f04946e735
CordobaFacundo <facundo.cordoba2012@gmail.com>
2025-07-25 12:09:40 -0300

    express first route

---
+++ index.js
@@ -0,0 +1,24 @@
+//Iniciamos con npm run dev que activa nodemon
+
+const express = require('express');
+require('dotenv').config(); //Cargar las variables de entorno desde el archivo .env
+
+
+
+
+
+//Crear el servidor de express
+const app = express();
+
+//Directorio público
+app.use(express.static('public'));
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'));
+//CRUD de eventos
+
+
+//Escuchar peticiones HTTP
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+});
