d875e62b7ebb7cced4cf9d51103eb2aa161185b3
CordobaFacundo <facundo.cordoba2012@gmail.com>
2025-09-03 11:48:37 -0300

    bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,31 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 
 const crearUsuario = async(req, res = response) => {
-    //const { name, email, password } = req.body;
+    const { email, password } = req.body;
     //console.log(req);
 
     try {
-        const usuario = new Usuario(req.body);
-    
+        let usuario = await Usuario.findOne({ email});
+        
+        if ( usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            });
+        }
+
+        usuario = new Usuario(req.body);
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt );
         await usuario.save();
     
         res.status(201).json({
             ok: true,
-            msg: 'registro',
+            uid: usuario.id,
+            name: usuario.name
         });
         
     } catch (error) {
